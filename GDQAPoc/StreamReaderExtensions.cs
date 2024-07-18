e2e87257b37e8f6006cd457e0e95b5225d0c84e4
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2024-07-18 23:41:00 +0200

    Add ReadUntil

--- GDQAPoc/StreamReaderExtensions.cs
+++ GDQAPoc/StreamReaderExtensions.cs
@@ -26,4 +26,44 @@ public static class StreamReaderExtensions
 				return true;
 		}
 	}
+
+	public static IAsyncEnumerable<ArraySegment<char>> ReadUntil(this StreamReader reader, char c)
+		=> ReadUntil(reader, c.ToString());
+
+	public static async IAsyncEnumerable<ArraySegment<char>> ReadUntil(this StreamReader reader, string s)
+	{
+		var arr = new char[4096];
+		var memory = arr.AsMemory();
+
+		int correct = 0;
+
+		while (true)
+		{
+			var read = await reader.ReadAsync(memory);
+			if (read == 0)
+				yield break;
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (arr[i] == s[correct])
+					correct++;
+				else
+					correct = 0;
+
+				if (correct == s.Length)
+				{
+					yield return new(arr, 0, i + 1);
+
+					// abcdefghijlmnoPQRSTuvwxyz
+					// 0                        ^read (Position)
+					// 0                 ^i
+					//                    ^Position -= read - i - 1
+					reader.BaseStream.Seek(-(read - i - 1), SeekOrigin.Current);
+					yield break;
+				}
+			}
+
+			yield return new(arr);
+		}
+	}
 }
