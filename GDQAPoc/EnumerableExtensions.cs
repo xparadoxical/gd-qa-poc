6abe9b9f9aaca7aedfbfdd8d0892accfe9969904
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2024-08-14 06:25:42 +0200

    tf did I do

--- GDQAPoc/EnumerableExtensions.cs
+++ GDQAPoc/EnumerableExtensions.cs
@@ -1,6 +1,9 @@
 ﻿namespace GDQAPoc;
 public static class EnumerableExtensions
 {
+	public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> e)
+		=> e.Where(x => x is not null).Select(x => x!);
+
 	public static T? FirstOfTypeOrDefault<T>(this System.Collections.IEnumerable e)
 		=> e.Cast<T>().FirstOrDefault();
 }
