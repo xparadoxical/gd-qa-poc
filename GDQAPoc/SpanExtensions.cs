4bcb763cb1e181c49783e330a6e12589c57a5a16
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2024-11-17 04:11:46 +0100

    CommunityToolkit trolled me

--- GDQAPoc/SpanExtensions.cs
+++ GDQAPoc/SpanExtensions.cs
@@ -1,11 +1,10 @@
-﻿using CommunityToolkit.HighPerformance;
-
-namespace GDQAPoc;
+﻿namespace GDQAPoc;
 public static class SpanExtensions
 {
 	/// <summary>Consumes a part of the <paramref name="span"/>, modifying it.</summary>
 	/// <returns>The consumed part of <paramref name="span"/>.</returns>
 	public static ReadOnlySpan<T> ConsumeUntil<T>(this scoped ref ReadOnlySpan<T> span, T terminator)
+		where T : IEquatable<T>
 	{
 		var length = span.IndexOf(terminator) + 1;
 		var consumed = span[..length];
